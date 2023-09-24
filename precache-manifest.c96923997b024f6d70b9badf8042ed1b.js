a8fac431abb6a4e8928995dc458ef022c262784a
Mr Amir <138612293+1234MrAmir@users.noreply.github.com>
2023-09-25 00:15:53 +0530

    Updates

---
+++ precache-manifest.c96923997b024f6d70b9badf8042ed1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a97c7ec8b3f6c2445e62",
+    "url": "/TextUtilsApp/static/css/main.67055f04.chunk.css"
+  },
+  {
+    "revision": "a97c7ec8b3f6c2445e62",
+    "url": "/TextUtilsApp/static/js/main.a97c7ec8.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtilsApp/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtilsApp/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "571b2e5b2c7737423df6",
+    "url": "/TextUtilsApp/static/js/runtime~main.571b2e5b.js"
+  },
+  {
+    "revision": "1d5e02c9286f9392d4ea5ced11b87e95",
+    "url": "/TextUtilsApp/index.html"
+  }
+];
